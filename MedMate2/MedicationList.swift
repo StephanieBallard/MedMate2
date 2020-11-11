43a167aef839ef9164ddaec3652558a11db86270
Stephanie <user.name>
2020-11-11 15:14:43 -0800

    initial commit, created initial view controller programmatically

---
+++ MedMate2/MedicationList.swift
@@ -0,0 +1,22 @@
+//
+//  MedicationList.swift
+//  MedMate2
+//
+//  Created by Stephanie Ballard on 11/11/20.
+//
+
+import UIKit
+
+class MedicationList: UIViewController {
+    
+    // MARK: - Properties -
+    
+    // MARK: - Lifecycle -
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+    }
+    
+    // MARK: - Helpers -
+}
